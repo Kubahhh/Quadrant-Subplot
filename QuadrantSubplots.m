0f50372a3a68329d30178f7bb6c10316591c61a2
Kubahhh <117305570+Kubahhh@users.noreply.github.com>
2022-11-09 13:22:55 -0500

    Add files via upload

---
+++ QuadrantSubplots.m
@@ -0,0 +1,20 @@
+subplot(2,2,1)
+x = linspace(0,25);
+y1 = x+2;
+plot(x,y1)
+title('Subplot 1')
+
+subplot(2,2,2)
+y2 = x+40;
+plot(x,y2)
+title('Subplot 2')
+
+subplot(2,2,3)
+y3 = x+800;
+plot(x,y3)
+title('Subplot 3')
+
+subplot(2,2,4)
+y4 = x+16000;
+plot(x,y4)
+title('Subplot 4')
